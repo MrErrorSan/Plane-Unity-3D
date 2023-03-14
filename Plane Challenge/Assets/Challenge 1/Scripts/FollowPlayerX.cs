dcb5bc4e55ef72e26fa494a5cba246e4700cc465
Muhammad Saad Shafiq <muhammadsaadshafiq275@gmail.com>
2023-03-14 19:08:34 +0500

    1st

---
+++ Plane Challenge/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject plane;
+    public Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = plane.transform.position + offset;
+    }
+}
